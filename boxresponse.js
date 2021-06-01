294f73235e24561b782e471fc3ebe9e90615bcd9
quentin <quentin.lefort@epita.fr>
2021-06-01 19:35:09 +0200

    [FRONTEND][LINK]
    add basic link, need to ctrl + click to make that work, maybe its because chrome isnt my main navigator

--- boxresponse.js
+++ boxresponse.js
@@ -7,21 +7,27 @@
     userDiv.className = "user response";
     userDiv.innerHTML = `<class="avatar"><span>${input}</span>`;
     messagesContainer.appendChild(userDiv);
+    
   
     let botDiv = document.createElement("div");
     let botImg = document.createElement("img");
     let botText = document.createElement("span");
+
     botDiv.id = "bot";
     botImg.src = "img/" + response.get_pictogram();
     botImg.width = 64;
     botImg.height = 64;
-    botImg.resi
     botImg.className = "avatar";
     botDiv.className = "bot response";
     botText.innerText = "En train d'ecrire...";
-    botDiv.appendChild(botText);
     botDiv.appendChild(botImg);
+    botDiv.appendChild(botText);
     messagesContainer.appendChild(botDiv);
+    
+
+
+    
+
     // Keep messages at most recent
     //messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 
@@ -29,10 +35,13 @@
     // Fake delay to seem "real"
     
     setTimeout(() => {
-      botText.innerText = `${response.get_text()}`; 
+      botText.innerText = `${response.get_text() + '\n'}`; 
+      var str = response.get_link();
+      var result = str.link(response.get_link());
+      botText.innerHTML += result;
       //reponse instantanee en vocal
       //textToSpeech(product)
-    }, 2000
+    }, 1000
     )
     
 }
