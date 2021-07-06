8ad217b3f10f756d0d0daa03f71f1a84edcb038e
Sylvain Keosouk <sylvain.keosouk@epita.fr>
2021-07-06 17:22:17 +0200

    modif: all.js refacto function addChat call to two functions user and bot

--- all.js
+++ all.js
@@ -121,39 +121,46 @@ function compare(promptsArray, repliesArray, string) {
     }
     return reply;
 }
-  
-function addChat(input, product) {
-    const messagesContainer = document.getElementById("messages");
-  
-    let userDiv = document.createElement("div");
-    userDiv.id = "user";
-    userDiv.className = "user response";
-    userDiv.innerHTML = `<class="avatar"><span>${input}</span>`;
-    messagesContainer.appendChild(userDiv);
-  
+
+function addChatBot(bot_message, messagesContainer) {
     let botDiv = document.createElement("div");
+    let botMessageDiv = document.createElement("div");
     let botImg = document.createElement("img");
     let botText = document.createElement("span");
+
     botDiv.id = "bot";
     botImg.src = "img/doctolib.png";
     botImg.className = "avatar";
-    botDiv.className = "bot response";
+    // botDiv.className = "bot response";
     botText.innerText = "En train d'ecrire...";
     botDiv.appendChild(botText);
-    // botDiv.appendChild(botImg);
+    botDiv.appendChild(botImg);
     messagesContainer.appendChild(botDiv);
     // Keep messages at most recent
-    //messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 
   
     // Fake delay to seem "real"
-    
     setTimeout(() => {
-      botText.innerText = `${product}`;
+      botText.innerText = `${bot_message}`;
       //textToSpeech(product)
     }, 2000
     )
-    
+}
+
+function addChatUser(user_message, messagesContainer) {
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.className = "user response";
+    userDiv.innerHTML = `<class="avatar"><span>${user_message}</span>`;
+
+    messagesContainer.appendChild(userDiv);
+}
+
+function addChat(user_message, bot_message) {
+    const messagesContainer = document.getElementById("messages");
+  
+    addChatUser(user_message, messagesContainer);
+    addChatBot(bot_message, messagesContainer);
 }
 
 function output(input) {
