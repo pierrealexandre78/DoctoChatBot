416e68b1f26ea562fbc02e78edb336d1bbecb047
Sylvain Keosouk <sylvain.keosouk@epita.fr>
2021-07-07 15:33:29 +0200

    modif css js html embedded popup

--- content-script.js
+++ content-script.js
@@ -1,3 +1,1 @@
-var div=document.createElement("div");
-document.body.appendChild(div);
-div.innerText="test123";
+window.open("index.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
