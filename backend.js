dabd759f003cd408137b066045af4f1af5f10bcb
Sylvain Keosouk <sylvain.keosouk@epita.fr>
2021-07-07 17:34:10 +0200

    backend.js, add new function for popup

--- backend.js
+++ backend.js
@@ -24,10 +24,10 @@ function compare(promptsArray, repliesArray, string) {
 function keyword_to_response(input) {
     let product;
     let response;
-  
+
     let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
 
-    if (compare(prompts, replies, text)) { 
+    if (compare(prompts, replies, text)) {
       // Search for exact match in `prompts`
       product = compare(prompts, replies, text);
     } else if (text.match(/thank/gi)) {
@@ -35,7 +35,7 @@ function keyword_to_response(input) {
     } else if (text.match(/(corona|covid|virus)/gi)) {
       // If no match, check if message contains `coronavirus`
       product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-    }  
+    }
       else if (text.match(/(motdepasse|mot de passe|mdp|m d p)/gi)) {
       // If no match, check if message contains `mot de passe`
       product = mdp[Math.floor(Math.random() * mdp.length)];
@@ -48,7 +48,40 @@ function keyword_to_response(input) {
       // If all else fails: random alternative
       product = alternative[Math.floor(Math.random() * alternative.length)];
     }
-  
+
     // Update DOM
     addBoxResponse(input, product, response);
+}
+
+function send_response(input) {
+    let product;
+    let response;
+
+    let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+
+    if (compare(prompts, replies, text)) {
+      // Search for exact match in `prompts`
+      product = compare(prompts, replies, text);
+    } else if (text.match(/thank/gi)) {
+      product = "You're welcome!"
+    } else if (text.match(/(corona|covid|virus)/gi)) {
+      // If no match, check if message contains `coronavirus`
+      product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+    }
+      else if (text.match(/(motdepasse|mot de passe|mdp|m d p)/gi)) {
+      // If no match, check if message contains `mot de passe`
+      product = mdp[Math.floor(Math.random() * mdp.length)];
+      response = new Response("Pour changer votre mot de passe :", "Logo-Cadenas.jpg", "https://www.doctolib.fr/account/passwords/new");
+    } else if (text.match(/(pharmacie)/gi)) {
+      response = new Response("Pour trouver une pharmacie :", "Logo-Pharmacie.jpg", "https://www.doctolib.fr/pharmacie");
+      product = response.get_text() + " " + response.get_link();
+    }
+     else {
+      // If all else fails: random alternative
+      product = alternative[Math.floor(Math.random() * alternative.length)];
+    }
+
+    return response;
+    // Update DOM
+    // addBoxResponse(input, product, response);
 }
