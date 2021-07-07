a8758a37ae680e801d11694bf0dcd7536b8529c8
Sylvain Keosouk <sylvain.keosouk@epita.fr>
2021-07-07 15:17:24 +0200

    change js, css, html

--- TODO.md
+++ TODO.md
@@ -1,3 +1,7 @@
 # TODO
 1. Find Chatbot template / API
 2. Connect it to content.js
+
+### Front
+-Change chat box collapse when reducing window size
+-fix chat box on window
--- background.js
+++ background.js
@@ -12,5 +12,22 @@ chrome.runtime.onInstalled.addListener(function() {
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
-  
+
 });
+
+
+// chrome.app.runtime.onLaunched.addListener(function() {
+//   var windowWidth = 300;
+//   var windowHeight = 325;
+//   chrome.app.window.create('window.html', {
+//     outerBounds: { // 'bounds' is deprecated, and you want full window size
+//       width: windowWidth,
+//       height: windowHeight,
+//       left: screen.availWidth - windowWidth,
+//       top: screen.availHeight - windowHeight,
+//     },
+//     resizable: false,
+//     frame: 'none'
+//   });
+// });
+//
