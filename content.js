c232692e7210c816564469b9a4cd515fa1fe47c8
Sylvain Keosouk <sylvain.keosouk@epita.fr>
2021-07-09 19:47:24 +0200

    content.js, increase popup size; popup.js add href link and line breaks every 36 chars

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
-window.open("popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+window.open("popup.html", "extension_popup", "width=800, height=750,status=no,scrollbars=yes,resizable=no");
 window.focus();
--- popup.js
+++ popup.js
@@ -1,119 +1,127 @@
 $(function() {
-  var INDEX = 0;
-  $("#chat-submit").click(function(e) {
-    e.preventDefault();
-    var msg = $("#chat-input").val();
-    if(msg.trim() == ''){
-      return false;
-    }
-    generate_message(msg, 'self');
-    var buttons = [
-        {
-          name: 'Existing User',
-          value: 'existing'
-        },
-        {
-          name: 'New User',
-          value: 'new'
+    var INDEX = 0;
+    $("#chat-submit").click(function(e) {
+        e.preventDefault();
+        var msg = $("#chat-input").val();
+        if(msg.trim() == ''){
+            return false;
         }
-      ];
-    setTimeout(function() {
-      generate_message(msg, 'user');
-    }, 1000)
+        generate_message(msg, 'self');
+        var buttons = [
+            {
+                name: 'Existing User',
+                value: 'existing'
+            },
+            {
+                name: 'New User',
+                value: 'new'
+            }
+        ];
+        setTimeout(function() {
+            generate_message(msg, 'user');
+        }, 1000)
 
-  })
+    })
 
-  function generate_message(msg, type) {
-    INDEX++;
-    var str="";
-    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
-    str += "          <span class=\"msg-avatar\">";
-
-    // User
-    if (type == "self") {
-        str += "            <img src=\"img\/avatar.png\">";
-        str += "          <\/span>";
-        str += "          <div class=\"cm-msg-text\">";
-        str += msg;
+    function addNewlines(str) {
+        var result = '';
+        while (str.length > 0) {
+            result += str.substring(0, 36) + '\n';
+            str = str.substring(36);
+        }
+        return result;
     }
 
-    // Bot
-    else {
-        str += "            <img src=\"img\/doctolib.png\">";
-        str += "          <\/span>";
-        str += "          <div class=\"cm-msg-text\">";
-        // str += "<a href=\"https://www.doctolib.fr/\naccount/passwords/new\"> https://www.doctolib.fr/\naccount/passwords/new";
+    function generate_message(msg, type) {
+        INDEX++;
+        var str="";
+        str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
+        str += "          <span class=\"msg-avatar\">";
 
-        response = keyword_to_response(msg);
+        // User
+        if (type == "self") {
+            str += "            <img src=\"img\/avatar.png\">";
+            str += "          <\/span>";
+            str += "          <div class=\"cm-msg-text\">";
+            str += msg;
+        }
 
-        if (response.get_text())
-            str += response.get_text();
+        // Bot
+        else {
+            str += "            <img src=\"img\/doctolib.png\">";
+            str += "          <\/span>";
+            str += "          <div class=\"cm-msg-text\">";
 
-        if (response.get_link())
-            str += response.get_link();
+            response = keyword_to_response(msg);
+
+            if (response.get_text())
+                str += addNewlines(response.get_text());
+
+            if (response.get_link())
+                str += "<a href=\"" + addNewlines(response.get_link()) + "\">" + addNewlines(response.get_link()) + "";
+        }
+        str += "          <\/div>";
+        str += "        <\/div>";
+        $(".chat-logs").append(str);
+        $("#cm-msg-"+INDEX).hide().fadeIn(300);
+        if(type == 'self'){
+            $("#chat-input").val('');
+        }
+        $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
     }
-    str += "          <\/div>";
-    str += "        <\/div>";
-    $(".chat-logs").append(str);
-    $("#cm-msg-"+INDEX).hide().fadeIn(300);
-    if(type == 'self'){
-     $("#chat-input").val('');
-    }
-    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
-  }
 
-  // function generate_button_message(msg, buttons){
-  //   /* Buttons should be object array
-  //     [
-  //       {
-  //         name: 'Existing User',
-  //         value: 'existing'
-  //       },
-  //       {
-  //         name: 'New User',
-  //         value: 'new'
-  //       }
-  //     ]
-  //   */
-  //   INDEX++;
-  //   var btn_obj = buttons.map(function(button) {
-  //      return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
-  //   }).join('');
-  //   var str="";
-  //   str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
-  //   str += "          <span class=\"msg-avatar\">";
-  //   str += "            <img src=\"https:\/\/www.filalapat.fr\/sites\/default\/files\/2020-07\/age_chat_trouve.jpg\">";
-  //   str += "          <\/span>";
-  //   str += "          <div class=\"cm-msg-text\">";
-  //   str += msg;
-  //   str += "          <\/div>";
-  //   str += "          <div class=\"cm-msg-button\">";
-  //   str += "            <ul>";
-  //   str += btn_obj;
-  //   str += "            <\/ul>";
-  //   str += "          <\/div>";
-  //   str += "        <\/div>";
-  //   $(".chat-logs").append(str);
-  //   $("#cm-msg-"+INDEX).hide().fadeIn(300);
-  //   $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
-  //   $("#chat-input").attr("disabled", true);
-  // }
+    // function generate_button_message(msg, buttons){
+    //   /* Buttons should be object array
+    //     [
+    //       {
+    //         name: 'Existing User',
+    //         value: 'existing'
+    //       },
+    //       {
+    //         name: 'New User',
+    //         value: 'new'
+    //       }
+    //     ]
+    //   */
+    //   INDEX++;
+    //   var btn_obj = buttons.map(function(button) {
+    //      return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
+    //   }).join('');
+    //   var str="";
+    //   str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
+    //   str += "          <span class=\"msg-avatar\">";
+    //   str += "            <img src=\"https:\/\/www.filalapat.fr\/sites\/default\/files\/2020-07\/age_chat_trouve.jpg\">";
+    //   str += "          <\/span>";
+    //   str += "          <div class=\"cm-msg-text\">";
+    //   str += msg;
+    //   str += "          <\/div>";
+    //   str += "          <div class=\"cm-msg-button\">";
+    //   str += "            <ul>";
+    //   str += btn_obj;
+    //   str += "            <\/ul>";
+    //   str += "          <\/div>";
+    //   str += "        <\/div>";
+    //   $(".chat-logs").append(str);
+    //   $("#cm-msg-"+INDEX).hide().fadeIn(300);
+    //   $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
+    //   $("#chat-input").attr("disabled", true);
+    // }
 
-  $(document).delegate(".chat-btn", "click", function() {
-    var value = $(this).attr("chat-value");
-    var name = $(this).html();
-    $("#chat-input").attr("disabled", false);
-    generate_message(name, 'self');
-  })
+    $(document).delegate(".chat-btn", "click", function() {
+        var value = $(this).attr("chat-value");
+        var name = $(this).html();
+        $("#chat-input").attr("disabled", false);
+        generate_message(name, 'self');
+    })
 
-  $("#chat-circle").click(function() {
-    $("#chat-circle").toggle('scale');
-    $(".chat-box").toggle('scale');
-  })
+    $("#chat-circle").click(function() {
+        $("#chat-circle").toggle('scale');
+        $(".chat-box").toggle('scale');
+    })
 
-  $(".chat-box-toggle").click(function() {
-    $("#chat-circle").toggle('scale');
-    $(".chat-box").toggle('scale');
-  })
+    $(".chat-box-toggle").click(function() {
+        $("#chat-circle").toggle('scale');
+        $(".chat-box").toggle('scale');
+    })
 
 })
