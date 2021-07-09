65c638189ec19250e2d36290c9c49a988681a64a
Sylvain Keosouk <sylvain.keosouk@epita.fr>
2021-07-09 18:37:42 +0200

    add avatar.png for user in popup.js

--- popup.js
+++ popup.js
@@ -54,42 +54,42 @@ $(function() {
     $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
   }
 
-  function generate_button_message(msg, buttons){
-    /* Buttons should be object array
-      [
-        {
-          name: 'Existing User',
-          value: 'existing'
-        },
-        {
-          name: 'New User',
-          value: 'new'
-        }
-      ]
-    */
-    INDEX++;
-    var btn_obj = buttons.map(function(button) {
-       return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
-    }).join('');
-    var str="";
-    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
-    str += "          <span class=\"msg-avatar\">";
-    str += "            <img src=\"https:\/\/www.filalapat.fr\/sites\/default\/files\/2020-07\/age_chat_trouve.jpg\">";
-    str += "          <\/span>";
-    str += "          <div class=\"cm-msg-text\">";
-    str += msg;
-    str += "          <\/div>";
-    str += "          <div class=\"cm-msg-button\">";
-    str += "            <ul>";
-    str += btn_obj;
-    str += "            <\/ul>";
-    str += "          <\/div>";
-    str += "        <\/div>";
-    $(".chat-logs").append(str);
-    $("#cm-msg-"+INDEX).hide().fadeIn(300);
-    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
-    $("#chat-input").attr("disabled", true);
-  }
+  // function generate_button_message(msg, buttons){
+  //   /* Buttons should be object array
+  //     [
+  //       {
+  //         name: 'Existing User',
+  //         value: 'existing'
+  //       },
+  //       {
+  //         name: 'New User',
+  //         value: 'new'
+  //       }
+  //     ]
+  //   */
+  //   INDEX++;
+  //   var btn_obj = buttons.map(function(button) {
+  //      return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
+  //   }).join('');
+  //   var str="";
+  //   str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
+  //   str += "          <span class=\"msg-avatar\">";
+  //   str += "            <img src=\"https:\/\/www.filalapat.fr\/sites\/default\/files\/2020-07\/age_chat_trouve.jpg\">";
+  //   str += "          <\/span>";
+  //   str += "          <div class=\"cm-msg-text\">";
+  //   str += msg;
+  //   str += "          <\/div>";
+  //   str += "          <div class=\"cm-msg-button\">";
+  //   str += "            <ul>";
+  //   str += btn_obj;
+  //   str += "            <\/ul>";
+  //   str += "          <\/div>";
+  //   str += "        <\/div>";
+  //   $(".chat-logs").append(str);
+  //   $("#cm-msg-"+INDEX).hide().fadeIn(300);
+  //   $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
+  //   $("#chat-input").attr("disabled", true);
+  // }
 
   $(document).delegate(".chat-btn", "click", function() {
     var value = $(this).attr("chat-value");
